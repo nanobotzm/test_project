3606ece173d14fa778eedc6abf8ac2e3c60f43cd
exy_p <exy_pret@gmail.com>
2021-03-20 19:43:31 +0200

    sixth commit

---
+++ src/accounts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    name = 'Подписчики'
+    verbose_name = 'Подписчики'
--- src/myapp/apps.py
+++ src/myapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MyappConfig(AppConfig):
     name = 'myapp'
-    verbose_name='Приложение в обучающих целях'
+    verbose_name = 'Приложение в обучающих целях'
